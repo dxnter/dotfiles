5a77d970c82d053142f60433940734f0f108cffe
Danny Foster <danny.foster@northwestern.edu>
2022-03-10 10:45:52 -0600

    Change to Nerd font

--- installs/4-nerd-fonts.sh
+++ installs/4-nerd-fonts.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-read -p "Install Nerd Fonts? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "[-] Installing nerd-fonts (may take awhile)"
-  git clone https://github.com/ryanoasis/nerd-fonts --depth 1 /tmp/nerd-fonts
-  pushd /tmp/nerd-fonts && ./install.sh
-  popd
-fi
+echo "[-] Installing Hack nerd-font"
+git clone https://github.com/ryanoasis/nerd-fonts --depth 1 /tmp/nerd-fonts
+pushd /tmp/nerd-fonts && ./install.sh Hack && popd
