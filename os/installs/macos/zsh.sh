2ecfa59241e204d08a14b21f48fb4e9b5e2053b5
Danny Foster <danny@foster.sh>
2022-03-13 21:52:14 -0500

    Modified zsh install

--- os/installs/macos/zsh.sh
+++ os/installs/macos/zsh.sh
@@ -18,8 +18,11 @@ install_zsh_plugin() {
 }
 
 zsh() {
+
     print_info "• Shell Configuration"
+
     brew_install "zsh" "zsh"
+
     [[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(which zsh)"
     if ! grep "$zsh_path" /etc/shells; then
         print_info "Adding $zsh_path to /etc/shells"
@@ -31,23 +34,45 @@ zsh() {
         print_info "Default shell changed to $zsh_path"
     fi
 
-    source "$HOME/.zshrc"
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     print_info "oh-my-zsh"
-    git clone --depth=1 git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --quiet
+
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        execute "git clone --depth=1 git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --quiet" \
+            "oh-my-zsh"
+    else
+        print_info "oh-my-zsh already installed => skipping"
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     print_info "oh-my-zsh plugins"
-    install_zsh_plugin https://github.com/asdf-vm/asdf.git asdf
-    install_zsh_plugin https://github.com/Aloxaf/fzf-tab fzf-tab
-    install_zsh_plugin https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting
-    install_zsh_plugin https://github.com/zsh-users/zsh-autosuggestions.git zsh-autosuggestions
 
-    if [ ! -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
+    execute "install_zsh_plugin https://github.com/asdf-vm/asdf.git asdf" \
+        "asdf"
+
+    execute "install_zsh_plugin https://github.com/Aloxaf/fzf-tab fzf-tab" \
+        "fzf-tab"
+
+    execute "install_zsh_plugin https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting" \
+        "zsh-syntax-highlighting"
+
+    execute "install_zsh_plugin https://github.com/zsh-users/zsh-autosuggestions.git zsh-autosuggestions" \
+        "zsh-autosuggestions"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
       print_info "powerline10k theme"
-      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+      execute "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" \
+        "powerline10k theme"
     else
       print_info "powerline10k theme already installed => skipping"
     fi
+
+    source "$HOME/.zshrc"
+
  }
 
 zsh
