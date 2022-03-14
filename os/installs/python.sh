ae8c6f47596e083eeb46ad67e5709a1786e4c24d
Danny Foster <danny@foster.sh>
2022-03-13 22:22:36 -0500

    Update asdf installs

--- os/installs/python.sh
+++ os/installs/python.sh
@@ -5,10 +5,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-python3_version="3.9.2"
+python3_version="3.10.2"
 
 print_info "• Python"
 
+
+asdf uninstall python
 asdf plugin add python
 asdf install python "${python3_version}"
 asdf global python "${python3_version}"
