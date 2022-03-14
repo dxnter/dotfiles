5e6298170321a14f3a9971b01f284a62341ea392
Danny Foster <danny@foster.sh>
2022-03-13 22:11:13 -0500

    Python fix

--- os/installs/python.sh
+++ os/installs/python.sh
@@ -6,13 +6,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 python2_version="2.7.14"
-python3_version="3.9.1"
+python3_version="3.9.2"
 
 print_info "• Python"
 
 asdf plugin add python
 asdf install python "${python2_version}"
-asdf install python "${python3_version}"
-asdf global python "${python3_version} ${python2_version}"
+LDFLAGS="-L/usr/local/opt/bzip2/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/openssl@1.1/lib" CFLAGS="-I/usr/local/opt/bzip2/include -I/usr/local/opt/zlib/include -I/usr/local/opt/openssl@1.1/include -I$(xcrun --show-sdk-path)/usr/include -Wno-implicit-function-declaration" asdf install python "${python3_version}"
+
+LDFLAGS="-L/usr/local/opt/bzip2/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/openssl@1.1/lib" CFLAGS="-I/usr/local/opt/bzip2/include -I/usr/local/opt/zlib/include -I/usr/local/opt/openssl@1.1/include -I$(xcrun --show-sdk-path)/usr/include -Wno-implicit-function-declaration" asdf global python "${python3_version} ${python2_version}"
 
 print_success "Python ${python2_version} & ${python3_version} installed"
