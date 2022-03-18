c2b1d9685bd234d40a52262cbaa28c212ccc87db
Danny Foster <danny.foster@northwestern.edu>
2022-03-17 19:32:12 -0500

    Modify nodejs install to use absolute asdf bin

--- system/installs/nodejs.sh
+++ system/installs/nodejs.sh
@@ -9,9 +9,9 @@ nodejs_version="16.13.2"
 
 print_info "• Node.js"
 
-asdf plugin remove nodejs
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs "${nodejs_version}"
-asdf global nodejs "${nodejs_version}"
+~/.asdf/bin/asdf plugin remove nodejs
+~/.asdf/bin/asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+~/.asdf/bin/asdf install nodejs "${nodejs_version}"
+~/.asdf/bin/asdf global nodejs "${nodejs_version}"
 
 print_success "Node.js ${nodejs_version} installed"
