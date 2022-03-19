547a2615b45efde13d89f4e68ea6ee4a8c9ed520
Danny Foster <danny@foster.sh>
2022-03-19 00:51:05 -0500

    Missing quotes

--- system/installs/poetry.sh
+++ system/installs/poetry.sh
@@ -10,6 +10,6 @@ print_info "• Poetry"
 if type poetry >/dev/null 2>&1; then
   print_info "Poetry already installed => skipping"
 else
-  execute "curl -sSL https://install.python-poetry.org | $(resolve_bin python3) -" \
+  execute "curl -sSL https://install.python-poetry.org | $(resolve_bin "python3") -" \
       "Installing Poetry"
 fi
