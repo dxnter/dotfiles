3c1931070fb5891b1c25e8bdc77077684e55943b
Danny Foster <danny@foster.sh>
2023-06-01 18:55:13 -0500

    fix: install Docker cask

--- system/installs/dev_tools.sh
+++ system/installs/dev_tools.sh
@@ -8,15 +8,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_info "• Development Tools"
 
 # CLI Tools
-brew_install "Docker" "docker"
+brew_install "Docker" "docker" "--cask"
 brew_install "Docker Compose" "docker-compose"
 brew_install "HTTP Server" "http-server"
 brew_install "Redis" "redis"
+brew_install "PHP" "php"
 
 # GUI Tools
 brew_install "Visual Studio Code" "visual-studio-code" "--cask"
 brew_install "Jetbrains Toolbox" "jetbrains-toolbox" "--cask"
-brew_install "PHP Monitor" "phpmon" "--cask"
 brew_install "iTerm 2" "iterm2" "--cask"
 brew_install "DBeaver" "dbeaver-community" "--cask"
 brew_install "RapidAPI" "rapidapi" "--cask"
